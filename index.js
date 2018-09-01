f2f1c6b0f4727f5d4ea9ae0692ec857f5d8d87dd
Khai <tttkhai93@gmail.com>
2018-09-01 00:49:13 -0400

    change environment

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const keys = require('./config/keys');
 const passport = require('passport');
 const cookieSession = require('cookie-session');
 const PORT = process.env.PORT || 5000;
-require('./db/db')(mongoose);
+// require('./db/db')(mongoose);
 require('./models/User');
 require('./services/passport');
-
 
 const app = express();
 
@@ -22,7 +21,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
-
+const environment = process.env.NODE_ENV || 'developement';
 app.listen(PORT,
-  console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT} and ${environment}`)
 );
